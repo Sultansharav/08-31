91ea4c5b7137c03e4ebe15c851b8c7f43ab2c5f7
Sultansharav <54223375+Sultansharav@users.noreply.github.com>
2019-09-08 08:55:03 +0800

    Update d18.py

--- Дасгалууд/d18.py
+++ Дасгалууд/d18.py
@@ -1,7 +1,7 @@
 # m тоо хүртэл хэдэн анхны тоо байгааг ол
 
 n = int(input())
-
+count = 0
 for i in range(2, n + 1):
     k = 0
     for j in range (2, i // 2 + 1):
@@ -9,15 +9,6 @@ for i in range(2, n + 1):
             k += 1
     if k == 0:
         print(i)
-count = i.count("i")
+        count +=1
 
-print(count)
-
-'''
-string = input()
-substring = "o"
-
-count = string.count(substring)
-
-print("ene vgend orson o vsgiin too: ", count)
-'''
+print(n, "hurtelh anhny toonuudyn too ni: ", count)
